8653308989968643a3b2bfdbf37d381c3d2d788b
Jegan Arockiaraj <a.jegan@gmail.com>
2017-10-09 21:12:01 -0700

    integrating react-d3-speedometer component

---
+++ src/component/Speedometer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactSpeedometer from "react-d3-speedometer";
+
+export default class  Speedometer extends Component {
+
+
+
+render(){
+return (
+  <ReactSpeedometer/>
+)
+}
+}
