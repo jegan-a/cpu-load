3c362e31d7e52f40af37afded9781086523910e3
Jegan Arockiaraj <a.jegan@gmail.com>
2017-10-11 09:37:49 -0700

    testing server sent event with time

---
+++ server/app.js
@@ -0,0 +1,29 @@
+var express = require('express')
+  , app = express()
+  , sse = require('./sse')
+  , os = require('os');
+
+var connections = []
+  , votes = {yes: 0, no: 0}
+
+app.engine('jade', require('jade').__express) //__
+app.set('view engine', 'jade')
+
+app.use(sse)
+
+
+
+app.get('/stream', function(req, res) {
+    res.sseSetup()
+    setInterval(function() {
+    let time = new Date().toLocaleTimeString()
+    res.sseSend(time)
+  }, 5000)
+
+
+  connections.push(res)
+})
+
+app.listen(3001, function() {
+  console.log('Listening on port 3001...')
+})
