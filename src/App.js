2733c3117db8f33688dcde1a31ccb49aac471456
Jegan Arockiaraj <a.jegan@gmail.com>
2017-10-11 17:12:25 -0700

    clean ups

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Live CPU Load Monitor</h1>
         </header>
         <p className="App-intro">
           <ReactSpeedometer />
